a6e113a26135838e0cc5165e9c3adda3ef4fb495
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-12-05 16:38:03 +0700

    feat: create Navigation component #184

---
+++ nextjs/foodzero/src/utils/testUtils.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+import { render } from '@testing-library/react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+// Themes
+import { customTheme } from '@themes/index'
+
+export const customRender = (component: ReactNode) => {
+  return render(
+    <ChakraProvider theme={customTheme}>{component}</ChakraProvider>,
+  )
+}
+
+export * from '@testing-library/react'
+export * from '@testing-library/jest-dom'
+export { customRender as render }
