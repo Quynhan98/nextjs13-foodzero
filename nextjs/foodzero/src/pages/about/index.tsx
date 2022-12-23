752a12233494a5705553a629ae3dbaa74ad38762
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-12-23 17:45:39 +0700

    feat: add Process component #210

--- nextjs/foodzero/src/pages/about/index.tsx
+++ nextjs/foodzero/src/pages/about/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, FormEvent, ChangeEvent, useMemo } from 'react'
+import { useState, useCallback, FormEvent, ChangeEvent } from 'react'
 import Image from 'next/image'
 import { Box, Flex, Heading, Text, useToast } from '@chakra-ui/react'
 
@@ -10,11 +10,11 @@ import {
   RESERVATION_TIME,
   SERVER_ERROR,
   SNACKBAR_BOOKING_SUCCESS,
-  SOPHISTICATED_PROCESS,
 } from '@constants/index'
 
 // Components
 import ReservationForm from '@components/ReservationForm'
+import Process from '@components/Process'
 
 // Hooks
 import { useBookingContext } from '@hooks/useBookingContext'
@@ -78,113 +78,6 @@ const About = () => {
         ...selectValues,
       }))
     },
-    [],
-  )
-
-  const renderSteps = useMemo(
-    () => (
-      <>
-        <Flex
-          flexDirection={{ base: 'column', md: 'row' }}
-          justifyContent={{ base: 'center', md: 'space-between' }}
-          gap={{ base: '10px', md: '102px' }}
-          pt={{ base: '30px', md: '174px' }}
-          alignItems="center"
-          margin={{ base: '0 auto', md: 'unset' }}
-        >
-          <Box
-            width={{ base: '277px', md: '792px' }}
-            height={{ base: '233px', md: '638px' }}
-            position="relative"
-          >
-            <Image
-              fill
-              src={SOPHISTICATED_PROCESS.stepOne.imageUrl}
-              alt="split ribs picture"
-              sizes="(max-width: 768px) 277px, 233px
-  (min-width: 1200px) 792px, 638px"
-            />
-          </Box>
-          <Box maxW={{ base: '277px', md: '616px' }}>
-            <Heading as="h4" size="medium" variant="secondary">
-              01.{SOPHISTICATED_PROCESS.stepOne.title}
-            </Heading>
-            <Text pt={{ base: '8px', md: '28px' }}>
-              {SOPHISTICATED_PROCESS.stepOne.description}
-            </Text>
-          </Box>
-        </Flex>
-        <Flex
-          pt={{ base: '50px', md: '118px' }}
-          flexDirection={{ base: 'column', md: 'row-reverse' }}
-          justifyContent="space-between"
-        >
-          <Box margin={{ base: '0 auto', md: 'unset' }}>
-            <Box
-              width={{ base: '277px', md: '650px' }}
-              height={{ base: '227px', md: '533px' }}
-              position="relative"
-            >
-              <Image
-                fill
-                src={SOPHISTICATED_PROCESS.stepTwo.imageUrl}
-                alt="marinated ribs picture"
-                sizes="(max-width: 768px) 277px, 227px
-  (min-width: 1200px) 650px, 533px"
-              />
-            </Box>
-            <Heading
-              as="h4"
-              size="medium"
-              variant="secondary"
-              pt={{ base: '8px', md: '46px' }}
-              ml={{ base: '0px', md: '80px' }}
-            >
-              02.{SOPHISTICATED_PROCESS.stepTwo.title}
-            </Heading>
-            <Text
-              pt={{ base: '8px', md: '28px' }}
-              maxW={{ base: '277px', md: '415px' }}
-              ml={{ base: '0px', md: '80px' }}
-            >
-              {SOPHISTICATED_PROCESS.stepTwo.description}
-            </Text>
-          </Box>
-          <Flex
-            pl={{ base: '0px', md: '71px' }}
-            pt={{ base: '50px', md: '150px' }}
-            flexDirection={{ base: 'column-reverse', md: 'column' }}
-            margin={{ base: '0 auto', md: 'unset' }}
-            gap={{ base: '10px', md: 'unset' }}
-          >
-            <Box pb={{ base: '20px', md: '46px' }}>
-              <Heading as="h4" size="medium" variant="secondary">
-                03.{SOPHISTICATED_PROCESS.stepThree.title}
-              </Heading>
-              <Text
-                pt={{ base: '8px', md: '28px' }}
-                maxW={{ base: '277px', md: '432px' }}
-              >
-                {SOPHISTICATED_PROCESS.stepThree.description}
-              </Text>
-            </Box>
-            <Box
-              width={{ base: '277px', md: '650px' }}
-              height={{ base: '217px', md: '509px' }}
-              position="relative"
-            >
-              <Image
-                fill
-                src={SOPHISTICATED_PROCESS.stepThree.imageUrl}
-                alt="grilled ribs picture"
-                sizes="(max-width: 768px) 277px, 217px
-  (min-width: 1200px) 650px, 509px"
-              />
-            </Box>
-          </Flex>
-        </Flex>
-      </>
-    ),
     [],
   )
 
@@ -344,9 +237,8 @@ const About = () => {
       <Box
         as="video"
         controls
-        autoPlay
         src={INTRO_VIDEO}
-        poster="/images/introPoster.webp"
+        poster="/images/introPoster.jpg"
         objectFit="cover"
         width="100%"
         height="100%"
@@ -397,7 +289,7 @@ const About = () => {
         >
           Sophisticated Process
         </Heading>
-        {renderSteps}
+        <Process />
       </Box>
 
       {/* Reservation Section */}
