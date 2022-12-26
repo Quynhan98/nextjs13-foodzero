05a97218de53f420890c9e4b62d9662c355d5a48
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-12-26 17:58:22 +0700

    feat: add 404 page

---
+++ nextjs/foodzero/src/pages/404.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { Center, Text } from '@chakra-ui/react'
+
+// Constants
+import { API_ERROR_NOT_FOUND } from '@constants/index'
+
+const NotFound = () => (
+  <Center minH="90vh" flexDirection="column">
+    <Text paddingBottom="50px" variant="warning" size="extraLarge">
+      {API_ERROR_NOT_FOUND}
+    </Text>
+    <Link href="/">
+      <Text color="blue" size="common">
+        Back To Home
+      </Text>
+    </Link>
+  </Center>
+)
+
+export default NotFound
