abac2bdf6b7d4e197240104be6ce33ad50291457
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2023-01-30 17:03:07 +0700

    deploy

--- nextjs/foodzero/stories/BlogAvatar.stories.tsx
+++ nextjs/foodzero/stories/BlogAvatar.stories.tsx
@@ -9,7 +9,7 @@ export default { title: 'Components/BlogAvatar', component: BlogAvatar }
 export const Avatar = {
   args: {
     src: AVATAR_MOCK,
-    name: 'Julie Christie test 4165465465',
+    name: 'Julie Christie storybook deploy',
     date: '2022-12-06T23:29:35.000Z',
     numberOfComments: 2,
   },
